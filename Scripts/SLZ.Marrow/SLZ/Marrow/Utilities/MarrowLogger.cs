ff435cc737558f82c484d71a8bea9be5ee240eb5
notnotnotswipez <strubb06@gmail.com>
2024-07-19 17:47:34 -0400

    Push fixes

--- Scripts/SLZ.Marrow/SLZ/Marrow/Utilities/MarrowLogger.cs
+++ Scripts/SLZ.Marrow/SLZ/Marrow/Utilities/MarrowLogger.cs
@@ -1,38 +1,10 @@
 using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
-using Palmmedia.ReportGenerator.Core.Logging;
 
 namespace SLZ.Marrow.Utilities
 {
 	public static class MarrowLogger
 	{
-		private static ILoggerFactory _loggerFactory
-		{
-			[CompilerGenerated]
-			get
-			{
-				return null;
-			}
-		}
-
-		[PublicAPI]
-		public static ILogger Logger
-		{
-			[CompilerGenerated]
-			get
-			{
-				return null;
-			}
-		}
-
-		static MarrowLogger()
-		{
-		}
-
-		[PublicAPI]
-		public static ILogger GetLogger(string categoryName)
-		{
-			return null;
-		}
+		
 	}
 }
